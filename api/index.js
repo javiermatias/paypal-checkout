842ff9285dfe0b3ad0922fe7ac247d5e6c5d7056
Javier Matias <javierjimenez78@gmail.com>
2024-12-12 14:11:39 -0300

    add dotenv

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import fetch from "node-fetch";
-import "dotenv/config";
+require('dotenv').config();
 import path from "path";
 import {userExists, insertUser, newClient,newSenderPrueba } from './userdb.js';
 // Import necessary modules
